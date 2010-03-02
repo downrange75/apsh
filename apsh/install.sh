4c6d3e0373a9d61e07bd8d28916d58a7cc07e45d
rutledge.chris <rutledge.chris@b00f3c2a-2169-11df-8298-5f372d8d1cf2>
2010-03-02 17:18:29 +0000

    added conditional statements and outputting to the install script

--- apsh/install.sh
+++ apsh/install.sh
@@ -5,10 +5,27 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-mkdir -p /etc/apsh /usr/local/lib/perl
+if [ ! -d /usr/local/lib/perl ]
+then
+   echo "Creating directory /usr/local/lib/perl..."
+   mkdir -p /usr/local/lib/perl
+fi
 
+if [ ! -d /etc/apsh ]
+then
+   echo "Creating directory /etc/apsh..."
+   mkdir -p /etc/apsh
+fi
+
+echo "Installing apsh and apscp..."
 cp apsh apscp /usr/local/bin/.
+echo "Installing APSH.pm..."
 cp APSH.pm /usr/local/lib/perl/.
-cp nodes.tab /etc/apsh
+
+if [ ! -f /etc/apsh/nodes.tab ]
+then
+   echo "Installing base nodes.tab..."
+   cp nodes.tab /etc/apsh
+fi
 
 chmod +x /usr/local/bin/apsh /usr/local/bin/apscp
